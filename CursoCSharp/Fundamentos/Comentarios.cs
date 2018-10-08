e79e06bd3a83dbcb6a41fb8beaf4e225c4ef68cf
Rayetsu <gabriel25asilva@gmail.com>
2018-10-08 23:01:06 +0100

    Variáveis e Constantes - Fundamentos

---
+++ CursoCSharp/Fundamentos/Comentarios.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.Fundamentos
+{
+
+    class Comentarios
+    {
+        /// <summary>
+        /// XML Comments
+        /// </summary>
+        public static void Executar()
+        {
+            // Cuidado  com os comentários desnecessários
+            Console.WriteLine("Código claro é sempre melhor!");
+
+            /*
+             * Comentário de
+             * múltiplas linhas
+             */
+            Console.WriteLine("O C# tem o XML Comments");
+
+            //ctr + k, ctr + c ---> Comentar
+            //ctr + k, ctr + u ---> Descomentar
+            Console.WriteLine("1");
+            Console.WriteLine("2");
+            // ctr + d ---> Duplica a linha onde o cursor está
+        }
+    }
+}
---
+++ CursoCSharp/Fundamentos/VariaveisEConstantes.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.Fundamentos
+{
+    class VariaveisEConstantes
+    {
+        public static void Executar()
+        {
+            //area da circunferencia
+            double raio = 4.5;
+            const double PI = 3.14;
+
+            raio = 5.5;
+            //PI = 3.1415;
+
+            double area = PI * raio * raio;
+
+            Console.WriteLine(area);
+            /*soma so tem precedencia à concatenação se 
+             *tiver entre parenteses (area + 1000)
+            */
+            Console.WriteLine("Área é " + area);
+
+            bool estaChovendo = true;
+            Console.WriteLine("Está Chovendo " + estaChovendo);
+
+            byte idade = 45;
+            Console.WriteLine("Idade " + idade);
+
+            sbyte saldoDeGols = sbyte.MinValue;
+            Console.WriteLine("Saldo de Gols " + saldoDeGols);
+
+            short salario = short.MaxValue;
+            Console.WriteLine("Salário " + salario);
+
+            int menorValorInt = int.MinValue;
+            Console.WriteLine("Menor int " + menorValorInt);
+
+            uint populacaoBrasileira = 207_600_000;
+            Console.WriteLine("População Brasileira " + populacaoBrasileira);
+
+            long menorValorLong = long.MinValue;
+            Console.WriteLine("Menor Long " + menorValorLong);
+
+            ulong populacaoMundial = 7_600_000_000;
+            Console.WriteLine("População Mundial " + populacaoMundial);
+
+            float precoComputador = 1299.99f;
+            Console.WriteLine("Preço Computador " + precoComputador);
+
+            double valorDeMercadoApple = 1_000_000_000_000.00;
+            Console.WriteLine("Valor Apple " + valorDeMercadoApple);
+
+            decimal distanciaEntreEstrelas = decimal.MaxValue;
+            Console.WriteLine("Distância Estrelas " + distanciaEntreEstrelas);
+
+            char letra = 'a';
+            Console.WriteLine("Letra " + letra);
+
+            string texto = "Seja bem vindo ao curso C#!";
+            Console.WriteLine(texto);
+        }
+    }
+}
