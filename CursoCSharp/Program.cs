f838396d557af2308b243a5db3c5c5884d32c495
Rayetsu <gabriel25aSilva@gmail.com>
2019-07-04 00:38:15 +0100

    Progress...

--- CursoCSharp/Program.cs
+++ CursoCSharp/Program.cs
@@ -5,6 +5,7 @@ using CursoCSharp.Fundamentos;
 using CursoCSharp.EstruturasDeControle;
 using CursoCSharp.ClassesEMetodos;
 using CursoCSharp.Colecoes;
+using CursoCSharp.OO;
 
 namespace CursoCSharp {
     class Program {
@@ -67,6 +68,12 @@ namespace CursoCSharp {
                 { "Igualdade  - Coleções", Igualdade.Executar },
                 { "Stack  - Coleções", ColecoesStack.Executar },
                 { "Dictionary  - Coleções", ColecoesDictionary.Executar },
+
+                // OO
+                { "Herança  - OO", Heranca.Executar },
+                { "Construtor This  - OO", ConstrutorThis.Executar },
+                { "Encapsulamento  - OO", OO.Encapsulamento.Executar },
+                { "Polimorfismo  - OO", OO.Polimorfismo.Executar },
             });
 
             central.SelecionarEExecutar();
---
+++ Encapsulamento/SubCelebridade.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Encapsulamento
+{
+    public class SubCelebridade
+    {
+        // Todos
+        public string InfoPublica = "Tenho um instagram";
+
+        // Herança
+        protected string CorDoOlho = "Verde";
+
+        // Mesmo projeto (assembly)
+        internal ulong NumeroCelular = 5511999999;
+
+        // Herança ou mesmo projeto
+        protected internal string JeitoDefalar = "Uso muitas gírias";
+
+        // Mesma classe ou herança no mesmo projeto (c# >= 7.2)
+        private protected string SegredoFamilia = "Bla Bla";
+
+        // private é o padrão
+        bool UsaMuitoPhotoshop = true;
+
+        public void MeusAcessos() {
+            Console.WriteLine("Subcelebridade...");
+
+            Console.WriteLine(InfoPublica);
+            Console.WriteLine(CorDoOlho);
+            Console.WriteLine(NumeroCelular);
+            Console.WriteLine(JeitoDefalar);
+            Console.WriteLine(SegredoFamilia);
+            Console.WriteLine(UsaMuitoPhotoshop);
+        }
+    }
+}
