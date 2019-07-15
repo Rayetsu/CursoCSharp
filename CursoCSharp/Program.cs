cf1a124e2be2359fc1b623aa144e9b07d6f3daa0
Rayetsu <gabriel25aSilva@gmail.com>
2019-07-16 00:15:09 +0100

    Exceções

--- CursoCSharp/Program.cs
+++ CursoCSharp/Program.cs
@@ -7,6 +7,7 @@ using CursoCSharp.ClassesEMetodos;
 using CursoCSharp.Colecoes;
 using CursoCSharp.OO;
 using CursoCSharp.MetodosEFuncoes;
+using CursoCSharp.Excecoes;
 
 namespace CursoCSharp {
     class Program {
@@ -85,6 +86,10 @@ namespace CursoCSharp {
                 { "Usando Delegates  - Métodos e Funções", UsandoDelegates.Executar },
                 { "Delegates Como Função Anonima - Métodos e Funções", DelegateFuncAnonima.Executar },
                 { "Métodos de Extensão - Métodos e Funções", MetodosDeExtensao.Executar },
+
+                //Exceções
+                { "Primeira Exceção - Exceções", PrimeiraExcecao.Executar },
+                { "Exceções Personalizadas - Exceções", ExcecoesPersonalizadas.Executar },
             });
 
             central.SelecionarEExecutar();
